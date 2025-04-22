5e1e9f6b7807ad187653e83d690615db282697bc
PSGtatitos <tatitoschris@gmail.com>
2025-04-22 15:32:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ comp = ""
 
 for i in range(1):
     comp += random.choice(choose)
-
+print(comp)
 game = input("Rock (R) " \
 "Paper (P) " \
 "Scissors (S) ")
 
-if game == "R" and comp == "R":
+if game == "R" or "r" and comp == "R":
         print("It's a tie.")
-elif game == "R" and comp == "P":
+elif game == "R" or "r" and comp == "P":
     print("I won!!")
-elif game == "P" and comp == "R":
+elif game == "R" or "r" and comp == "S":
     print("You won.")
-elif game == "P" and comp == "P":
+elif game == "P" or "p" and comp == "R":
+    print("You won.")
+elif game == "P" or "p" and comp == "P":
         print("It's a tie.")
-elif game == "P" and comp == "S":
+elif game == "P" or "p" and comp == "S":
         print("I won!!")
-elif game == "S" and comp == "P":
+elif game == "S" or "s" and comp == "P":
         print("You won.")        
-elif game == "S" and comp == "S":
+elif game == "S" or "s" and comp == "S":
         print("It's a tie.")
-elif game == "S" and comp == "P":
-        print("I won!!")
-elif game == "S" and comp == "R":
-        print("You won.")        
+elif game == "S" or "s" and comp == "R":
+        print("I won!!")        
